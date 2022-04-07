7f78b5f2691ea60236f9ee3a9d9585c32ee5c03c
lyonqingmango <qingmango@gmail.com>
2022-04-06 19:17:12 -0700

    fixed again

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,7 +11,7 @@ var App = () => (
     </nav>
     <div className="row">
       <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> view goes here</h5></div>
+        <div><h5><em>videoPlayer</em> view go here</h5></div>
       </div>
       <div className="col-md-5">
         <div><h5><VideoList videos={exampleVideoData}/></h5></div>
